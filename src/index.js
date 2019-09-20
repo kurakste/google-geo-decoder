164fd21caefcccbff49d926cf4d32685dc41b0d3
Stepan <kurakste@gmail.com>
2019-09-20 22:11:39 +0300

    add api url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 
-function geoDecoder(lat, lon, key) {
+https = require('https');
+
+async function geoDecoder(lat, lon, key) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&${key}`;
-  
+  const resp = await https.get(url);
+  return resp;
 }
+
+module.exports = geoDecoder;
