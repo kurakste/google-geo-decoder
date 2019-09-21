af5d4dd9a449f8b91d96fe070e0b3faedfff4b71
Stepan <kurakste@gmail.com>
2019-09-21 15:56:09 +0300

    add dest

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
-const https = require('https');
 const axios = require('axios');
 
+
 async function geoDecoder(lat, lon, key) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&${key}`;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${key}`;
   const resp = await axios(url);
   return resp;
 }
--- test.js
+++ test.js
@@ -1,5 +1,27 @@
+const dotenv = require('dotenv');
 const geoDecoder = require('./src');
-(async function () {
-  const resp = await geoDecoder(40.714224,-73.961452, null);
-  console.log(resp.data);
-})();
+
+dotenv.config();
+
+const key = process.env.GGKEY;
+
+if (!key) throw new Error('Google map key doesn\'t found.');
+
+
+const locations = [
+  { name: 'nizhnekamsk', loc: [55.635270, 51.808362] },
+  { name: 'kazan', loc: [55.819027, 49.050113] },
+  { name: 'road tatarstan', loc: [55.664437, 49.854521] },
+  { name: 'road tatarstan', loc: [55.229049, 50.898795] },
+  { name: 'ufa city', loc: [54.798129, 56.040179] },
+  { name: 'ufa forest', loc: [54.791566, 55.992392] }
+];
+
+const [lon, lat] = locations[1].loc;
+
+async function main() {
+  const resp = await geoDecoder(lon, lat, key);
+  console.log(resp.data.results[1].formatted_address);
+}
+
+main();
